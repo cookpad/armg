2875759dd0beb5a66fc0a59a6f48767320bdc9e0
winebarrel <sgwr_dts@yahoo.co.jp>
2017-08-14 00:43:37 +0900

    Small fix

--- lib/armg/abstract_mysql_adapter_ext.rb
+++ lib/armg/abstract_mysql_adapter_ext.rb
@@ -1,7 +1,7 @@
 module Armg::AbstractMysqlAdapterExt
   def initialize_type_map(m)
     super
-    m.register_type %r(^geometry)i,  Armg::MysqlGeometry.new
+    m.register_type %r(^geometry)i, Armg::MysqlGeometry.new
   end
 
   def indexes(*args, &block)
