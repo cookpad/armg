a2d8cb1a87dce3c9506c098a957b8c2f278f5648
winebarrel <sgwr_dts@yahoo.co.jp>
2017-08-13 22:01:28 +0900

    Small fix

--- lib/armg/utils.rb
+++ lib/armg/utils.rb
@@ -1,10 +1,10 @@
 # see https://dev.mysql.com/doc/refman/5.7/en/gis-data-formats.html
 module Armg::Utils
   def mysql_geometry_to_wkb(mysql_geometry)
-    wkb = mysql_geometry.b
-    srid = wkb.slice!(0..3)
-    wkb[4] = "\x20"
-    wkb.insert(5, srid)
+    mysql_geometry = mysql_geometry.b
+    srid = mysql_geometry.slice!(0..3)
+    mysql_geometry[4] = "\x20"
+    mysql_geometry.insert(5, srid)
   end
   module_function :mysql_geometry_to_wkb
 
